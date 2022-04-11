42c16cbc60a92e10236c5a0d695327fa1f95ae26
Alban <alban.rouault@hotmail.com>
2022-04-11 20:45:44 +0200

    modification de la classe Weapon : - ajout du champ malusPrécision

--- src/main/java/net/cnam/object/Weapon.java
+++ src/main/java/net/cnam/object/Weapon.java
@@ -9,11 +9,13 @@ public class Weapon extends Object {
 //CHAMPS
     private int durability;
     private int power;
+    private int malusAccuracy;
 
 //CONSTRUCTEUR
-    public Weapon(int durability, int power) {
+    public Weapon(int durability, int power, int malusAccuracy) {
         this.durability = durability;
         this.power = power;
+        this.malusAccuracy = malusAccuracy;
     }
     
     
