bb215511dfef30db96ad618399619ad4e6d0271c
Alban <alban.rouault@hotmail.com>
2022-04-12 17:36:59 +0200

    quelques modifs peu importantes

--- src/main/java/net/cnam/object/PortableObject.java
+++ src/main/java/net/cnam/object/PortableObject.java
@@ -4,5 +4,8 @@ public interface PortableObject {
     
     //permet à l'objet d'être tenu par une entité
     public void isCarry();
+    
+    //lache l'objet dans la pièce
+    public void drop();
 
 }
