71ffa4b6a0af1b3a3661d70eed50a980e18cd5b2
Alban <alban.rouault@hotmail.com>
2022-04-14 15:11:25 +0200

    Ajout du block border

---
+++ src/main/java/net/cnam/structure/block/Border.java
@@ -0,0 +1,13 @@
+package net.cnam.structure.block;
+
+/**
+ *
+ * @author Rouault Alban (alban.rouault.auditeur@lecnam.net)
+ */
+public class Border extends Block {
+
+    public Border(char caracter) {
+        super(caracter);
+    }
+
+}
