2f4d9e9fb8618a8409721e2c9276741b0f1a0968
FarmVivi <farmvividev@gmail.com>
2022-04-21 12:49:47 +0200

    Add methods in StringUtils

--- src/main/java/net/cnam/utils/StringUtils.java
+++ src/main/java/net/cnam/utils/StringUtils.java
@@ -4,6 +4,41 @@ package net.cnam.utils;
  * Classe utilitaire permettant de manipuler des chaines de caractères
  */
 public class StringUtils {
+
+    /**
+     * Méthode retournant un tableau de chaines de caractères en séparant celle
+     * passé en paramètre par les retours à la ligne
+     *
+     * @param string Chaine de caractères à séparer
+     * @return Un tableau de chaines de caractères
+     */
+    public static String[] convertStringToStringArray(String string) {
+        return string.split("\n");
+    }
+
+    /**
+     * Méthode retournant la chaine de caractère concaténé du tableau passé en
+     * paramètres
+     *
+     * @param table Le tableau des chaines de caractères
+     * @return Les chaines de caractère concaténé en une seule chaine
+     */
+    public static String convertStringArrayToString(String[] table) {
+        String output = "";
+
+        for (int i = 0; i < table.length; i++) {
+            String str = table[i];
+
+            output += str;
+
+            if (i != table.length - 1) {
+                output += "\n";
+            }
+        }
+
+        return output;
+    }
+
     /**
      * Méthode retournant la longueur maximale de chaines de caractères
      *
--- src/test/java/net/cnam/utils/StringUtilsTest.java
+++ src/test/java/net/cnam/utils/StringUtilsTest.java
@@ -9,6 +9,32 @@ import static org.junit.Assert.*;
 public class StringUtilsTest {
 
     /**
+     * Test of convertStringToStringArray method, of class StringUtils.
+     */
+    @Test
+    public void testConvertStringToStringArray() {
+        System.out.println("testConvertStringToStringArray");
+        String string = "ab\na\nabcdef\nabc";
+        String[] expResult = new String[]{"ab", "a", "abcdef", "abc"};
+        String[] result = StringUtils.convertStringToStringArray(string);
+        assertArrayEquals(expResult, result);
+        System.out.println("OK");
+    }
+
+    /**
+     * Test of convertStringArrayToString method, of class StringUtils.
+     */
+    @Test
+    public void testConvertStringArrayToString() {
+        System.out.println("testConvertStringArrayToString");
+        String[] table = new String[]{"ab", "a", "abcdef", "abc"};
+        String expResult = "ab\na\nabcdef\nabc";
+        String result = StringUtils.convertStringArrayToString(table);
+        assertEquals(expResult, result);
+        System.out.println("OK");
+    }
+
+    /**
      * Test of getMaximumLength method, of class StringUtils.
      */
     @Test
