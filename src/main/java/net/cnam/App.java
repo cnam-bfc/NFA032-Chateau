dd788fb439e158022aeddb0a851a93d8c8a355e9
FarmVivi <farmvividev@gmail.com>
2022-05-02 23:19:16 +0200

    debugGenerator

--- src/main/java/net/cnam/App.java
+++ src/main/java/net/cnam/App.java
@@ -1,15 +1,42 @@
 package net.cnam;
 
+import java.util.Random;
 import net.cnam.console.Console;
+import net.cnam.generator.Generator;
+import net.cnam.object.Location;
+import net.cnam.structure.Castle;
+import net.cnam.structure.Room;
+import net.cnam.structure.Stage;
 
 public class App {
 
     public static void main(String[] args) {
         System.out.println("En développement...");
 
-        Console console = new Console();
-        console.adjustSize();
+        // Console console = new Console();
+        // console.adjustSize();
         // console.debugKeys();
+
+        debugGenerator();
+    }
+
+    private static void debugGenerator() {
+        Generator gene = new Generator(new Random().nextLong());
+        Castle castle = gene.generateCastle();
+        Stage[] stages = castle.getStages();
+        for (int i = 1; i <= stages.length; i++) {
+            Stage stage = stages[i - 1];
+            System.out.println("-------------------------");
+            System.out.println("étage " + i + " (" + stage.getRooms().length + " pièces)");
+            Room[] rooms = stage.getRooms();
+            for (int y = 1; y <= rooms.length; y++) {
+                Room room = rooms[y - 1];
+                Location location = room.getLocation();
+                System.out.println("pièce " + y + " (x=" + location.getX() + " y=" + location.getY() + " length=" + room.getLength() + " width=" + room.getWidth() + ")");
+                System.out.println("Blocks 1ère dim = " + room.getBlocks().length);
+                System.out.println("Blocks 2ème dim = " + room.getBlocks()[0].length);
+            }
+        }
     }
 
     // Méthode pour effacer la console
