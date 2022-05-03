4a7c7be755b2ceaa14ed50d37f4f49350749220e
FarmVivi <farmvividev@gmail.com>
2022-05-03 16:10:50 +0200

    CButton

---
+++ src/main/java/net/cnam/gui/component/CButton.java
@@ -0,0 +1,65 @@
+package net.cnam.gui.component;
+
+import net.cnam.utils.StringUtils;
+
+public class CButton extends CLabel {
+
+    private boolean selected = false;
+    private boolean disabled = false;
+
+    public CButton(String text) {
+        super(text);
+    }
+
+    public CButton(String text, int length, int height) {
+        super(text, length, height);
+    }
+
+    public CButton(String[] lines) {
+        super(lines);
+    }
+
+    public CButton(String[] lines, int length, int height) {
+        super(lines, length, height);
+    }
+
+    @Override
+    public String[] render() {
+        String[] result = new String[this.getHeight()];
+        int line = 0;
+        String emptyLine = " ".repeat(this.getLength());
+
+        String[] textLines = StringUtils.convertStringToStringArray(this.getText());
+        // Lignes de la console - lignes de texte au millieu
+        int paddingHeight = this.getHeight() - textLines.length;
+        for (int i = 0; i < paddingHeight / 2; i++) {
+            line = this.renderAddLine(result, line, emptyLine);
+        }
+        for (String textLine : textLines) {
+            line = this.renderAddLine(result, line, StringUtils.centerString("\u001b[7m" + textLine + "\u001b[27m", ' ', this.getLength()));
+        }
+
+        // Bourage à la fin
+        for (; line < result.length;) {
+            line = renderAddLine(result, line, emptyLine);
+        }
+
+        return result;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    public void setDisabled(boolean disabled) {
+        this.disabled = disabled;
+    }
+}
