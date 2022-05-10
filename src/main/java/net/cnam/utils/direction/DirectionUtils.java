848f07dd382a3c1153ecfe0924bd7f554ec5fea7
FarmVivi <farmvividev@gmail.com>
2022-05-10 17:05:17 +0200

    DirectionUtils linux support

--- src/main/java/net/cnam/utils/direction/DirectionUtils.java
+++ src/main/java/net/cnam/utils/direction/DirectionUtils.java
@@ -1,8 +1,36 @@
 package net.cnam.utils.direction;
 
+import java.io.IOException;
+import net.cnam.utils.console.RawConsoleInput;
+
 public class DirectionUtils {
 
     public static Direction parseDirection(int input) throws DirectionNotFoundException {
+        try {
+            // Si un caractère d'échappemment est entré
+            if (input == 27 && RawConsoleInput.read(true) == 91) {
+                switch (RawConsoleInput.read(true)) {
+                    // 65 = Flèche du haut
+                    case 65 -> {
+                        return Direction.TOP;
+                    }
+                    // 66 = Flèche du bas
+                    case 66 -> {
+                        return Direction.BOTTOM;
+                    }
+                    // 67 = Flèche de droite
+                    case 67 -> {
+                        return Direction.RIGHT;
+                    }
+                    // 68 = Flèche de gauche
+                    case 68 -> {
+                        return Direction.LEFT;
+                    }
+                }
+            }
+        } catch (IOException ex) {
+        }
+
         switch (input) {
             // 57416 = Flèche haut ; 122 = z ; 90 = Z
             case 57416, 122, 90 -> {
