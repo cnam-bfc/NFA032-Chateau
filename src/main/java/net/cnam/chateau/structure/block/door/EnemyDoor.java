6cb3b856f5627df7edace3286a8b9955a745de94
Alban <alban.rouault@hotmail.com>
2022-05-25 11:33:52 +0200

    modif

--- src/main/java/net/cnam/chateau/structure/block/door/EnemyDoor.java
+++ src/main/java/net/cnam/chateau/structure/block/door/EnemyDoor.java
@@ -1,9 +1,28 @@
 package net.cnam.chateau.structure.block.door;
 
-/**
- *
- * @author Rouault Alban (alban.rouault.auditeur@lecnam.net)
- */
-public class EnemyDoor {
+import net.cnam.chateau.entity.enemy.Enemy;
+import net.cnam.chateau.gui.Console;
+import net.cnam.chateau.structure.Room;
+import net.cnam.chateau.structure.Stage;
+
+public class EnemyDoor extends Door {
+    
+    private Console console;
+    private Enemy enemy;
+
+    public EnemyDoor(Console console, Stage stage, Room roomOne, Room roomTwo) {
+        super(stage, roomOne, roomTwo);
+
+        this.console = console;
+    }
+
+    public EnemyDoor(Console console, Enemy enemy, Stage stage, Room roomOne, Room roomTwo) {
+        super(stage, roomOne, roomTwo);
+        
+        this.console = console;
+        this.enemy = enemy;
+    }
+    
+    
 
 }
