f6cc42e322d9c2fd64a69434196204e18e956c63
FarmVivi <farmvividev@gmail.com>
2022-05-25 22:45:11 +0200

    Stop music when fight stop

--- src/main/java/net/cnam/chateau/entity/Entity.java
+++ src/main/java/net/cnam/chateau/entity/Entity.java
@@ -24,9 +24,9 @@ import java.util.List;
 public abstract class Entity implements DisplayableObject {
     private static final int DEFAULT_HEALTH = 100;
     private static final int DEFAULT_RESISTANCE = 50;
-    private static final int DEFAULT_STRENGTH = 1000;
+    private static final int DEFAULT_STRENGTH = 15;
     private static final int DEFAULT_ACCURACY = 15;
-    private static final int DEFAULT_SPEED = 150;
+    private static final int DEFAULT_SPEED = 2;
 
     private Location location;
     private Stage stage;
