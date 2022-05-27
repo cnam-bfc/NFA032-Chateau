a36807e17dd829092ad05e945a8eebedd74a4756
FarmVivi <farmvividev@gmail.com>
2022-05-28 01:19:00 +0200

    Container fix

--- src/main/java/net/cnam/chateau/gui/play/container/ContainerMenu.java
+++ src/main/java/net/cnam/chateau/gui/play/container/ContainerMenu.java
@@ -5,10 +5,8 @@ import net.cnam.chateau.entity.Player;
 import net.cnam.chateau.gui.component.CChoices;
 import net.cnam.chateau.gui.component.CFrame;
 import net.cnam.chateau.gui.component.DisplayableComponent;
-import net.cnam.chateau.gui.component.SelectableComponent;
 import net.cnam.chateau.item.weapon.Weapon;
 import net.cnam.chateau.structure.block.container.Container;
-import net.cnam.chateau.utils.array.ArrayUtils;
 
 public class ContainerMenu extends CFrame implements DisplayableComponent {
     private boolean display = true;
@@ -16,41 +14,33 @@ public class ContainerMenu extends CFrame implements DisplayableComponent {
     public ContainerMenu(App app, Player player, Container block) {
         super(0, 0, block.getName());
 
-        SelectableComponent[] selectableComponent = new SelectableComponent[0];
+        CChoices choices = new CChoices(app, 1);
 
         if (player.hasItem() && block.hasItem() && !(block.getHiddenItem() instanceof Weapon)) {
-            selectableComponent = ArrayUtils.addOnBottomOfArray(selectableComponent,
-                    new ReplaceItemButton(app, this, player, block));
+            choices.add(new ReplaceItemButton(app, this, player, block));
         }
 
         if (player.hasWeapon() && block.hasItem() && (block.getHiddenItem() instanceof Weapon)) {
-            selectableComponent = ArrayUtils.addOnBottomOfArray(selectableComponent,
-                    new ReplaceWeaponButton(app, this, player, block));
+            choices.add(new ReplaceWeaponButton(app, this, player, block));
         }
 
         if (player.hasWeapon() && !block.hasItem()) {
-            selectableComponent = ArrayUtils.addOnBottomOfArray(selectableComponent,
-                    new PutWeaponButton(app, this, player, block));
+            choices.add(new PutWeaponButton(app, this, player, block));
         }
 
         if (player.hasItem() && !block.hasItem()) {
-            selectableComponent = ArrayUtils.addOnBottomOfArray(selectableComponent,
-                    new PutItemButton(app, this, player, block));
+            choices.add(new PutItemButton(app, this, player, block));
         }
 
         if (!player.hasItem() && block.hasItem() && !(block.getHiddenItem() instanceof Weapon)) {
-            selectableComponent = ArrayUtils.addOnBottomOfArray(selectableComponent,
-                    new TakeItemButton(app, this, player, block));
+            choices.add(new TakeItemButton(app, this, player, block));
         }
 
         if (!player.hasWeapon() && block.hasItem() && (block.getHiddenItem() instanceof Weapon)) {
-            selectableComponent = ArrayUtils.addOnBottomOfArray(selectableComponent,
-                    new TakeWeaponButton(app, this, player, block));
+            choices.add(new TakeWeaponButton(app, this, player, block));
         }
 
-        selectableComponent = ArrayUtils.addOnBottomOfArray(selectableComponent, new LeaveContainerButton(app, this));
-
-        CChoices choices = new CChoices(app, selectableComponent, 1);
+        choices.add(new LeaveContainerButton(app, this));
 
         this.getContentPane().getComponents().add(choices);
     }
