5c7fb3a976d64fb277246e89ed7a07d33407a776
FarmVivi <farmvividev@gmail.com>
2022-06-02 19:51:05 +0200

    ItemStats works for Wearables

--- src/main/java/net/cnam/chateau/item/wearable/Wearable.java
+++ src/main/java/net/cnam/chateau/item/wearable/Wearable.java
@@ -3,19 +3,18 @@ package net.cnam.chateau.item.wearable;
 import net.cnam.chateau.item.Item;
 
 public class Wearable extends Item {
-
     private int strength;
     private int accuracy;
     private int speed;
 
     /**
      * Constructeur
-     * 
-     * @param name          Nom de l'objet
-     * @param description   Description de l'objet
-     * @param strength      Statistique force de l'objet (bonus/malus)
-     * @param accuracy      Statistique précision de l'objet (bonus/malus)
-     * @param speed         Statistique rapidité de l'objet (bonus/malus)
+     *
+     * @param name        Nom de l'objet
+     * @param description Description de l'objet
+     * @param strength    Statistique force de l'objet (bonus/malus)
+     * @param accuracy    Statistique précision de l'objet (bonus/malus)
+     * @param speed       Statistique rapidité de l'objet (bonus/malus)
      */
     public Wearable(String name, String description, int strength, int accuracy, int speed) {
         super(name, description);
@@ -27,7 +26,7 @@ public class Wearable extends Item {
 
     /**
      * Getter permettant de récupérer la statistique de force de l'objet.
-     * 
+     *
      * @return la statistique force de l'objet (int)
      */
     public int getStrength() {
@@ -36,7 +35,7 @@ public class Wearable extends Item {
 
     /**
      * Setter permettant de définir la statistique de force de l'objet.
-     * 
+     *
      * @param strength la statistique force de l'objet (int)
      */
     public void setStrength(int strength) {
@@ -45,7 +44,7 @@ public class Wearable extends Item {
 
     /**
      * Getter permettant de récupérer la statistique de précision de l'objet.
-     * 
+     *
      * @return la statistique précision de l'objet (int)
      */
     public int getAccuracy() {
@@ -54,7 +53,7 @@ public class Wearable extends Item {
 
     /**
      * Setter permettant de définir la statistique de précision de l'objet.
-     * 
+     *
      * @param accuracy la statistique précision de l'objet (int)
      */
     public void setAccuracy(int accuracy) {
@@ -63,7 +62,7 @@ public class Wearable extends Item {
 
     /**
      * Getter permettant de récupérer la statistique de précision de l'objet.
-     * 
+     *
      * @return la statistique précision de l'objet (int)
      */
     public int getSpeed() {
@@ -72,7 +71,7 @@ public class Wearable extends Item {
 
     /**
      * Setter permettant de définir la statistique de rapidité de l'objet.
-     * 
+     *
      * @param speed la statistique rapidité de l'objet (int)
      */
     public void setSpeed(int speed) {
