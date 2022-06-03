c8ebcd01b1973aa2b69501399157851539083d8f
FarmVivi <farmvividev@gmail.com>
2022-06-03 11:48:12 +0200

    comms

--- src/main/java/net/cnam/chateau/gui/DisplayableObject.java
+++ src/main/java/net/cnam/chateau/gui/DisplayableObject.java
@@ -1,11 +1,15 @@
 package net.cnam.chateau.gui;
 
 /**
- * Interface permettant d'associer un caractère (char) à un objet
+ * Interface permettant d'associer un caractère à un objet.
+ *
+ * Un "caractère" est une chaine de caractères qui doit faire une longueur de 1 à l'affichage,
+ * elle peut par exemple contenir des couleurs qui vont augmenter la longueur de la chaine,
+ * mais à l'affichage la chaine sera toujours de longueur 1 sur l'écran
  */
 public interface DisplayableObject {
     /**
-     * Méthode permettant de récupérer le caractère (char) associé à l'objet
+     * Méthode permettant de récupérer le caractère associé à l'objet
      *
      * @return le caractère
      */
