66817688cabafe83e6e7aa5199d22056d783173c
FarmVivi <farmvividev@gmail.com>
2022-06-06 20:58:47 +0200

    DialogType history emote

--- src/main/java/net/cnam/chateau/gui/dialog/DialogType.java
+++ src/main/java/net/cnam/chateau/gui/dialog/DialogType.java
@@ -4,7 +4,7 @@ public enum DialogType {
     INFO("\u24D8 Information \u24D8", "Appuyez sur une touche pour continuer..."),
     WARNING("\u26A0 Attention \u26A0", "Appuyez sur une touche pour continuer..."),
     ERROR("\u26A0 Erreur \u26A0", "Appuyez sur une touche pour continuer..."),
-    HISTORY("\u24D8 Histoire \u24D8", "Appuyez sur une touche pour continuer..."),
+    HISTORY("\u270F Histoire \u270F", "Appuyez sur une touche pour continuer..."),
     EXCEPTION("\u26A0 Une erreur fatale est survenue \u26A0", "Appuyez sur une touche pour quitter...");
 
     private final String title;
